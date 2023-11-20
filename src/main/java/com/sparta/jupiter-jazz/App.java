aa5656bd5089926fcb9ebfe4a966ec98a8a59c3f
Mateusz Natan Molenda <91672079+Piasttorun@users.noreply.github.com>
2023-11-20 15:38:58 +0000

    Add files via upload
    Correct file structure

---
+++ src/main/java/com/sparta/jupiter-jazz/App.java
@@ -0,0 +1,13 @@
+package com.sparta.jupiter-jazz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
