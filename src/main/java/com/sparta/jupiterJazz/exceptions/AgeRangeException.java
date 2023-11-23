bf434508023b77ca3ae836324b581dd878de9657
richard <binghamrichard123@gmail.com>
2023-11-23 14:26:18 +0000

    Added exception classes for DAO exceptions

---
+++ src/main/java/com/sparta/jupiterJazz/exceptions/AgeRangeException.java
@@ -0,0 +1,7 @@
+package com.sparta.jupiterJazz.exceptions;
+
+public class AgeRangeException extends RuntimeException {
+    public AgeRangeException(String message) {
+        super(message);
+    }
+}
