e0764af6c9844b7956f50766818f4e2b3a3c327b
richard <binghamrichard123@gmail.com>
2023-11-23 14:28:00 +0000

    added exceptions to DTO interface method signatures

--- src/main/java/com/sparta/jupiterJazz/DAO.java
+++ src/main/java/com/sparta/jupiterJazz/DAO.java
@@ -1,5 +1,10 @@
 package com.sparta.jupiterJazz;
 
+import com.sparta.jupiterJazz.exceptions.AgeRangeException;
+import com.sparta.jupiterJazz.exceptions.DateRangeException;
+import com.sparta.jupiterJazz.exceptions.EmployeeIdException;
+import com.sparta.jupiterJazz.exceptions.LastNameException;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -9,11 +14,11 @@ public interface DAO {
 
     ArrayList<Employee> getEmployees(DTO dto);
 
-    Employee searchById(String id, DTO dto);
+    Employee searchById(String id, DTO dto) throws EmployeeIdException;
 
-    ArrayList<Employee> searchByLastName(String name, DTO dto);
+    ArrayList<Employee> searchByLastName(String name, DTO dto) throws LastNameException;
 
-    public ArrayList<Employee> searchByDateRange(Date firstDate, Date lastDate, DTO dto);
-    public ArrayList<Employee> searchByAgeRange(Date minDate, Date maxDate, DTO dto);
+    public ArrayList<Employee> searchByDateRange(Date firstDate, Date lastDate, DTO dto) throws DateRangeException;
+    public ArrayList<Employee> searchByAgeRange(Date minDate, Date maxDate, DTO dto) throws AgeRangeException;
 
 }
