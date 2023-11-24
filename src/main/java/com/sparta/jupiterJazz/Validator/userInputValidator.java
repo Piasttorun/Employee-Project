8a3432049ef8ab5ec4ba17a0af1e306dfeb86d94
richard <binghamrichard123@gmail.com>
2023-11-24 12:04:59 +0000

    added validation to userInputHandler

--- src/main/java/com/sparta/jupiterJazz/Validator/userInputValidator.java
+++ src/main/java/com/sparta/jupiterJazz/Validator/userInputValidator.java
@@ -1,28 +1,32 @@
 package com.sparta.jupiterJazz.Validator;
 
+import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class userInputValidator {
 
-    public static boolean validateNOEmployees(String userCommand){
-        return Integer.parseInt(userCommand) <= 10000 && Integer.parseInt(userCommand)>=1;
+    public static boolean validateNumEmployees(String userCommand){
+        return Integer.parseInt(userCommand) <= 1000 && Integer.parseInt(userCommand) >= 1;
     }
 
     public static boolean validateEmployeeID(String userCommand){
         return (userCommand.length() <= 8 && !userCommand.isEmpty());
     }
 
-    public static boolean validateDates(String userCommand){
-        return Pattern.compile("/^[0-9]{4}-[0-9]{2}-[0-9]{2}\\b\\s[0-9]{4}-[0-9]{2}-[0-9]{0,2}/").matcher(userCommand).find();
+    public static boolean validateDates(String firstDate, String secondDate){
+        return Pattern.compile("\\d{4}\\/\\d{2}\\/\\d{2}").matcher(firstDate).find()
+                && Pattern.compile("\\d{4}\\/\\d{2}\\/\\d{2}").matcher(secondDate).find();
         ///https://stackoverflow.com/questions/14862289/how-to-use-regex-in-java-to-pattern-match
 
     }
 
-    public static boolean validateAgeInput(String userCommand){
-        return Pattern.compile("/^[0-9]{0,2}\\b\\s[0-9]{0,2}/").matcher(userCommand).find();
+    public static boolean validateAgeInput(String firstAge, String secondAge){
+
+        return Pattern.compile("\\d{1,2}").matcher(firstAge).find()
+                && Pattern.compile("\\d{1,2}").matcher(secondAge).find();
     }
 
     public static boolean validateNameInput(String userCommand){
-        return Pattern.compile("/^[a-zA-z]+").matcher(userCommand).find();
+        return Pattern.compile("[a-zA-z]").matcher(userCommand).find();
     }
 }
